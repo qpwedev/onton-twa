7988f48061960c68503dae3177d7aced37d3892d
Nichita Gutu <gutunichita757@gmail.com>
2023-09-24 13:02:31 +0200

    add tonconnect button to roompage

--- src/pages/RoomPage.tsx
+++ src/pages/RoomPage.tsx
@@ -5,72 +5,7 @@ import { Member as MemberType } from "../types";
 
 import "./RoomPage.css";
 import { randomEmoji } from "../utils";
-
-// const mockMembers = [
-//   {
-//     id: 1,
-//     address: "0x1234567890123456789012345678901234567890",
-//     roomId: 1,
-//     createdAt: "2023-09-23T17:07:09.686Z",
-//     updatedAt: "2023-09-23T17:07:09.686Z",
-//   },
-//   {
-//     id: 2,
-//     address: "0x1234567890123456789012345678901234567890",
-//     roomId: 1,
-//     createdAt: "2023-09-23T17:07:10.847Z",
-//     updatedAt: "2023-09-23T17:07:10.847Z",
-//   },
-//   {
-//     id: 3,
-//     address: "0x1234567890123456789012345678901234567890",
-//     roomId: 1,
-//     createdAt: "2023-09-23T17:07:11.196Z",
-//     updatedAt: "2023-09-23T17:07:11.196Z",
-//   },
-//   {
-//     id: 4,
-//     address: "0x1234567890123456789012345678901234567890",
-//     roomId: 1,
-//     createdAt: "2023-09-23T17:07:11.426Z",
-//     updatedAt: "2023-09-23T17:07:11.426Z",
-//   },
-//   {
-//     id: 5,
-//     address: "0x1234567890123456789012345678901234567890",
-//     roomId: 1,
-//     createdAt: "2023-09-23T17:07:11.662Z",
-//     updatedAt: "2023-09-23T17:07:11.662Z",
-//   },
-//   {
-//     id: 6,
-//     address: "0x1234567890123456789012345678901234567890",
-//     roomId: 1,
-//     createdAt: "2023-09-23T17:07:11.886Z",
-//     updatedAt: "2023-09-23T17:07:11.886Z",
-//   },
-//   {
-//     id: 7,
-//     address: "0x1234567890123456789012345678901234567890",
-//     roomId: 1,
-//     createdAt: "2023-09-23T17:07:12.103Z",
-//     updatedAt: "2023-09-23T17:07:12.103Z",
-//   },
-//   {
-//     id: 8,
-//     address: "0x1234567890123456789012345678901234567890",
-//     roomId: 1,
-//     createdAt: "2023-09-23T17:07:12.363Z",
-//     updatedAt: "2023-09-23T17:07:12.363Z",
-//   },
-//   {
-//     id: 9,
-//     address: "0x1234567890123456789012345678901234567890",
-//     roomId: 1,
-//     createdAt: "2023-09-23T17:07:12.549Z",
-//     updatedAt: "2023-09-23T17:07:12.549Z",
-//   },
-// ];
+import { TonConnectButton } from "@tonconnect/ui-react";
 
 export default function RoomPage() {
   const roomId = window.location.pathname.split("/")[2];
@@ -81,6 +16,7 @@ export default function RoomPage() {
 
   return (
     <div className="room-page-wrapper">
+        <TonConnectButton />
       <div className="room-page-name-wrapper">
         <div className="room-page-name">{room?.name}</div>
         <div className="room-members-amount">{room.Members.length} members</div>
@@ -93,14 +29,6 @@ export default function RoomPage() {
     </div>
   );
 }
-
-// export type Member = {
-//   id: number;
-//   address: string;
-//   roomId: number;
-//   createdAt: string;
-//   updatedAt: string;
-// };
 
 function MembersList({ members }: { members: MemberType[] | null }) {
   return (
